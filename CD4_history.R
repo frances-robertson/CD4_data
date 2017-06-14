7fab48ae3bf13d6097852b5dbd5dca0f8073f358
Frances Robertson <francesrob@gmail.com>
2017-06-14 16:54:12 +0200

    initial project version

---
+++ CD4_history.R
@@ -0,0 +1,4 @@
+
+
+demog_data = read.xls ("Dropbox/HIV/demographics-scan-dates-hiv-controls-master-excel-Jan-2017.xlsx",sep=",")
+cd4scan_data_at9 = read.xls("/Volumes/MRI/UserFolders/Frances/!HIV_spreadsheets/2017/CD4_CD8_VL_20170131_final.xlsx",header=TRUE,sep=",")
